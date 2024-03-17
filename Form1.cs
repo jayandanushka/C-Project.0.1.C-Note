fd4a481f8630cf71be0dbee1dbd4fe70983dfd0e
User <User@DESKTOP-TJQ1PKG>
2024-03-17 12:00:20 +0530

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C_Note
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn
+        (
+            int nLeftRect,     // x-coordinate of upper-left corner
+            int nTopRect,      // y-coordinate of upper-left corner
+            int nRightRect,    // x-coordinate of lower-right corner
+            int nBottomRect,   // y-coordinate of lower-right corner
+            int nWidthEllipse, // height of ellipse
+            int nHeightEllipse // width of ellipse
+        );
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+
+            // Initialize timer
+            timer1 = new Timer();
+            timer1.Interval = 5000; // 5000 milliseconds = 5 seconds
+            timer1.Tick += timer1_Tick;
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            // Stop the timer to prevent multiple forms from opening
+            timer1.Stop();
+
+            // Open new form
+            Form2 newForm = new Form2();
+            newForm.Show();
+
+            // Close the current form
+            this.Hide(); // Use Hide() to hide the form without closing it
+        }
+    
+    }
+}
+
